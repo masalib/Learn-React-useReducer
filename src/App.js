8e64a77cda977aba80f9ed755e1828bfa1aab671
masanori <masalib@gmail.com>
2020-11-15 11:53:19 +0900

    useReducerを使っていない状態 単純なカウンタープログラム

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function changeCount(amount) {
+    setCount((prevCount) => prevCount + amount);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <span>{count}</span>
+      <div>
+        <button onClick={() => changeCount(1)}>+</button>
+        <button onClick={() => changeCount(-1)}>-</button>
+        <button onClick={() => resetCount()}>Reset</button>
+      </div>
+    </>
   );
 }
